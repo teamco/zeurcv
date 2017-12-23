d6d2c2c183fab8adff97834134bd9504b12da121
Vladimir Tkach <teamco@gmail.com>
2017-12-24 00:14:37 +0200

    fix users

--- client/views/admin/users/edit/profile/_profile.js
+++ client/views/admin/users/edit/profile/_profile.js
@@ -1,4 +1,7 @@
 import {getUserProfile} from '../../../../../../lib/users';
+import {subscribe} from '../../../../template';
+
+Template.editUserProfile.onCreated(() => subscribe(['users', 'userStatus', 'profile', 'roles']));
 
 Template.editUserProfile.helpers({
   selectedCountry: country => getUserProfile().userCountry === country
--- server/accounts/publish.js
+++ server/accounts/publish.js
@@ -1,7 +1,12 @@
 import {runAsAdmin} from '../../lib/users';
+import {accountProfile} from '../../model/accountProfile.model';
 
 Meteor.publish('users', function() {
   return runAsAdmin(this.userId, Meteor.users.find());
+});
+
+Meteor.publish('profile', function() {
+  return runAsAdmin(this.userId, accountProfile.find());
 });
 
 Meteor.publish('userStatus', function() {
