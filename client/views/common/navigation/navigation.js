ba7b818b18e3c23434d5fa14891e7723171d5a6f
Vladimir Tkach <teamco@gmail.com>
2017-12-28 10:57:33 +0200

    fix header

--- client/views/common/navigation/navigation.js
+++ client/views/common/navigation/navigation.js
@@ -1,4 +1,21 @@
-Template.navigation.rendered = function(){
-    // Initialize metisMenu
-    $('#side-menu').metisMenu();
+import {getUser} from '../../../../lib/users';
+
+Template.navigation.onRendered = function() {
+  // Initialize metisMenu
+  $('#side-menu').metisMenu();
 };
+
+Template.navHeader.helpers({
+
+  /**
+   * @method getUser
+   */
+  user: getUser
+});
+
+Template.navUserProfile.events({
+
+  'click #sign-out-profile': () => Meteor.logout(() => {
+    // TODO (teamco): Do something after logout
+  })
+});
