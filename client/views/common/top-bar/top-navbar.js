380339ba712042c4df1d209415b5247d89b78163
Vladimir Tkach <teamco@gmail.com>
2018-01-11 10:29:40 +0200

    fix landing page

--- client/views/common/top-bar/top-navbar.js
+++ client/views/common/top-bar/top-navbar.js
@@ -1,41 +1,46 @@
-Template.topNavbar.rendered = function(){
+Template.topNavbar.rendered = function() {
 
-    // FIXED TOP NAVBAR OPTION
-    // Uncomment this if you want to have fixed top navbar
-    // $('body').addClass('fixed-nav');
-    // $(".navbar-static-top").removeClass('navbar-static-top').addClass('navbar-fixed-top');
+  // FIXED TOP NAVBAR OPTION
+  // Uncomment this if you want to have fixed top navbar
+  // $('body').addClass('fixed-nav');
+  // $(".navbar-static-top").removeClass('navbar-static-top').addClass('navbar-fixed-top');
 
 };
 
 Template.topNavbar.events({
 
-    // Toggle left navigation
-    'click #navbar-minimalize': function(event){
+  // Toggle right sidebar
+  'click .right-sidebar-toggle': () => {
+    $('#right-sidebar').toggleClass('sidebar-open');
+  },
 
-        event.preventDefault();
+  // Toggle left navigation
+  'click #navbar-minimalize': event => {
+    event.preventDefault();
 
-        // Toggle special class
-        $("body").toggleClass("mini-navbar");
+    const $body = $('body');
+    const $sideMenu = $('#side-menu');
 
-        // Enable smoothly hide/show menu
-        if (!$('body').hasClass('mini-navbar') || $('body').hasClass('body-small')) {
-            // Hide menu in order to smoothly turn on when maximize menu
-            $('#side-menu').hide();
-            // For smoothly turn on menu
-            setTimeout(
-                function () {
-                    $('#side-menu').fadeIn(400);
-                }, 200);
-        } else if ($('body').hasClass('fixed-sidebar')) {
-            $('#side-menu').hide();
-            setTimeout(
-                function () {
-                    $('#side-menu').fadeIn(400);
-                }, 100);
-        } else {
-            // Remove all inline style from jquery fadeIn function to reset menu state
-            $('#side-menu').removeAttr('style');
-        }
+    // Toggle special class
+    $body.toggleClass('mini-navbar');
+
+    // Enable smoothly hide/show menu
+    if (!$body.hasClass('mini-navbar') || $body.hasClass('body-small')) {
+      // Hide menu in order to smoothly turn on when maximize menu
+      $sideMenu.hide();
+      // For smoothly turn on menu
+      setTimeout(() => {
+        $sideMenu.fadeIn(400);
+      }, 200);
+    } else if ($body.hasClass('fixed-sidebar')) {
+      $sideMenu.hide();
+      setTimeout(() => {
+        $sideMenu.fadeIn(400);
+      }, 100);
+    } else {
+      // Remove all inline style from jquery fadeIn function to reset menu state
+      $sideMenu.removeAttr('style');
     }
+  }
 
 });
