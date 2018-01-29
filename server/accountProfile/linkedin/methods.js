8606931c96a4f51458acf4ec36fdcaf482bb3e2e
Vladimir Tkach <teamco@gmail.com>
2018-01-30 00:16:02 +0200

    cont profile

--- server/accountProfile/linkedin/methods.js
+++ server/accountProfile/linkedin/methods.js
@@ -1,3 +1,4 @@
+import {Meteor} from 'meteor/meteor';
 import {is403, throwError} from '../../../lib/logs';
 import {accountProfile} from '../../../model/accountProfile.model';
 
@@ -20,27 +21,48 @@ Meteor.methods({
      * @type {{people: {me}}}
      */
     const linkedin = Linkedin().init(user.services[provider].accessToken);
+    const bound = Meteor.bindEnvironment(callback => callback());
+    linkedin.people.me((error, $in) => bound(() => error ?
+        throwError(error) :
+        Meteor.call('updateUserProfileData', $in)));
+  },
 
-    linkedin.people.me((error, $in) => {
-      if (error) {
-        return throwError(error);
-      }
+  /**
+   * @method createUserProfileData
+   * @param $in
+   * @param version
+   */
+  createUserProfileData: function($in, version) {
+    $in.updated_at = new Date();
+    $in.created_at = new Date();
+    $in.version = version;
+    accountProfile.insert($in);
+  },
 
-      const isAvailableProfile = accountProfile.findOne({id: $in.id});
+  /**
+   * @method updateUserProfileData
+   * @param $in
+   */
+  updateUserProfileData: function($in) {
+    const profile = accountProfile.findOne({id: $in.id}, {sort: {DateTime: -1, limit: 1}});
 
-      if (isAvailableProfile) {
-        if (isAvailableProfile.createdAt < $in.updatedAt) {
-          accountProfile.insert($in);
-        } else {
-          accountProfile.update(
-              {_id: $in.id},
-              {$set: {'updated_at': new Date()}},
-              {multi: true}
-          );
-        }
+    if (profile) {
+
+      // Update period: once per 30 days
+      const refreshPeriod = 30;
+      const currentDate = Date.now();
+      const lastUpdate = new Date(profile.updated_at);
+      lastUpdate.setDate(lastUpdate.getDate() + refreshPeriod);
+
+      if (lastUpdate.getTime() > currentDate) {
+        // TODO (teamco): Check for changes
+        Meteor.call('createUserProfileData', $in, profile.version + 1);
       } else {
-        accountProfile.insert($in);
+        accountProfile.update({_id: $in.id}, {$set: {'updated_at': new Date()}}
+        );
       }
-    });
+    } else {
+      Meteor.call('createUserProfileData', $in, 1);
+    }
   }
 });
